b61531ef9ab31c5c41b7751c0055dcab96d603cf
Naveed Sultan <naveed1224@gmail.com>
2020-03-06 11:18:29 -0500

    fixed bugs

--- picture-convert-tool/main.py
+++ picture-convert-tool/main.py
@@ -18,7 +18,7 @@ tool.geometry('300x500')
 text1 = '' # from path
 text2 = '' # to path
 text3 = ''#picture being converted
-from_file_path1 = ''
+from_file_path = ''
 to_file_path = ''
 
 E1 = '' #height
@@ -26,7 +26,6 @@ E2 = '' # width
 v2 = IntVar() # zipfile selection
 v1 = IntVar() # normalize selection
 E3 = ''# char to replace
-
 v1.set(0) # default as none
 v2.set(0) # default as none
 
@@ -34,15 +33,24 @@ v2.set(0) # default as none
 
 def from_select_file():
     from_file_path = filedialog.askdirectory()
-    text1.config(text=from_file_path)
+    from_path_test_input.insert(0,from_file_path)
 
 def to_select_file():
     to_file_path = filedialog.askdirectory()
-    text2.config(text=to_file_path)
+    to_path_test_input.insert(0,to_file_path)
 
 def print_num():
     print(E1.get())
     print(E2.get())
+    print(from_file_path)
+    from_path = Path(from_file_path)
+    to_path = Path(to_file_path)
+    print(from_path_test_input.get())
+    print('Actual Path')
+    print(Path(from_path_test_input.get()))
+    print(from_path)
+    print(to_path)
+    print('hello')
 
 
 def radio_button_output():
@@ -53,35 +61,42 @@ def radio_button_output():
 
 def button_command():
     #start progress bar
-    print(from_file_path)
-    from_path = Path(from_file_path)
-    to_path = Path(to_file_path)
+    from_path = Path(from_path_test_input.get())
+    to_path = os.path.join(to_path_test_input.get(),"")
+    from_path = os.path.join(from_path,"")
+    print(from_path_test_input.get())
     print(from_path)
-    print('hello')
+    print(to_path)
 
     popup = tk.Toplevel()
     tk.Label(popup, text="Following Pictures are beoing converted:").grid(row=0,column=0)
     text3 = tk.Label(popup, text="----").grid(row=2, column=0)
     popup.geometry('200x200')
     dirs = os.listdir(from_path)
+    print('directory')
+    print(dirs)
     h = int(E1.get())
     w = int(E2.get())
     Image.MAX_IMAGE_PIXELS = None
 
     for item in dirs:
+        print(item)
         if os.path.isfile(from_path + item):
-            im = Image.open(from_path + item)
-            imResize = im.resize((w, h), Image.ANTIALIAS)
-            item_name = item
-            item_name = item_name.replace("{}".format(E3), "")
-            if v1 == 1:
-                item_name = item_name.lower()
-            elif v1 == 2:
-                item_name = item_name.upper()
-            else:
-                None
-            imResize.save(to_path + item_name, 'JPEG', quality=90)
-            text3.config(text=item_name)
+            try:
+                im = Image.open(from_path + item)
+                imResize = im.resize((w, h), Image.ANTIALIAS)
+                item_name = item
+                item_name = item_name.replace("{}".format(E3), "")
+                if v1 == 1:
+                    item_name = item_name.lower()
+                elif v1 == 2:
+                    item_name = item_name.upper()
+                else:
+                    None
+                imResize.save(to_path + item_name, 'JPEG', quality=90)
+                text3.config(text=item_name)
+            except:
+                print('other file found')
 
         #    print('Failed')
     popup.destroy()
@@ -89,19 +104,28 @@ def button_command():
 
 
 
-text1 = tk.Label(tool, text='----')
+from_path_test = Label(tool, text="Path")
+from_path_test.pack(anchor=W)
+from_path_test_input = Entry(tool, bd =2)
+from_path_test_input.pack(anchor=W)
 from_file_btn = tk.Button(tool, text='Select: From Folder', command=from_select_file)
-
-text2 = tk.Label(tool, text='----')
-to_file_btn = tk.Button(tool, text='Select: To Folder', command=to_select_file)
-
-
-
-text1.pack(anchor=W)
 from_file_btn.pack(anchor=W)
 
-text2.pack(anchor=W)
+to_path_test = Label(tool, text="Path")
+to_path_test.pack(anchor=W)
+to_path_test_input = Entry(tool, bd =2)
+to_path_test_input.pack(anchor=W)
+to_file_btn = tk.Button(tool, text='Select: To Folder', command=to_select_file)
 to_file_btn.pack(anchor=W)
+
+
+
+
+
+
+
+
+
 #enter height
 L1 = Label(tool, text="Height")
 L1.pack(anchor=W)
